9d3646304d06473fb53ee4db2d56b653a1935c13
Amara Im <amaraim@icloud.com>
2023-01-14 21:41:08 -0500

    changed background color

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
-window.addEventListener('load',() => {
-    const form = document.getElementById("new-task-form");
-    const input = document.querySelector("#new-task-input");
-    const list_el = document.getElementById("tasks");
+// window.addEventListener('load',() => {
+//     const form = document.getElementById("new-task-form");
+//     const input = document.querySelector("#new-task-input");
+//     const list_el = document.getElementById("tasks");
 
-    form.addEventListener('submit',(e) => {
-        e.preventDefault();
-        const task = input.value;
-        if (!task) {
-            alert("please provide task");
-        }
+//     form.addEventListener('submit',(e) => {
+//         e.preventDefault();
+//         const task = input.value;
+//         if (!task) {
+//             alert("please provide task");
+//         }
 
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-        task_content_el.innerText=task;
+//         const task_el = document.createElement("div");
+//         task_el.classList.add("task");
+//         const task_content_el = document.createElement("div");
+//         task_content_el.classList.add("content");
+//         task_content_el.innerText=task;
         
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type="text";
-        task_input_el.value=task;
-        task_input_el.setAttribute("readonly","readonly");
+//         const task_input_el = document.createElement("input");
+//         task_input_el.classList.add("text");
+//         task_input_el.type="text";
+//         task_input_el.value=task;
+//         task_input_el.setAttribute("readonly","readonly");
 
 
-        task_el.appendChild(task_content_el);
-        task_content_el.appendChild(task_input_el);
-        list_el.appendChild(task_el);
+//         task_el.appendChild(task_content_el);
+//         task_content_el.appendChild(task_input_el);
+//         list_el.appendChild(task_el);
 
 
-    })
+//     })
 
-})
+// })
 
 /* music container */
 
@@ -45,7 +45,7 @@ const progressContainer = document.getElementById('progress-container');
 const cover = document.getElementById('cover');
 
 // Song titles
-const songs = ['midsommar', 'wistful-rain'];
+let songs = ['blue-boi', 'purple-cat'];
 
 // Keep track of song
 let songIndex = 0;
@@ -117,6 +117,25 @@ function setProgress(e) {
   const duration = audio.duration;
 
   audio.currentTime = (clickX / width) * duration;
+}
+
+function changeMusicType(type) {
+  console.log(type)
+  if (type == 'cloud') {
+    songs = ['midsommar', 'wistful-rain'];
+    // const isPlaying = musicContainer.classList.contains('play');
+
+    songIndex = 0;
+    loadSong(songs[songIndex]);
+    playSong();
+
+  }
+  else if (type == 'lofi') {
+    songs = ['blue-boi', 'purple-cat'];
+    songIndex = 0;
+    loadSong(songs[songIndex]);
+    playSong();
+  }
 }
 
 // Event listeners
